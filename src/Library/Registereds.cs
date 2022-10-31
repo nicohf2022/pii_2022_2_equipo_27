7e683ac64d6497e7c9689c62ce6dfd1e306a1e9f
Franco Bascialla <francobascialla@gmail.com>
2022-10-30 23:18:14 -0300

    Se comentan y justifican las clases

--- src/Library/Registereds.cs
+++ src/Library/Registereds.cs
@@ -2,9 +2,21 @@ namespace Library
 {
     public class Registereds
     {
+        /// <summary>
+        /// Creamos los objetos de la lista en Registereds porque es la clase
+        /// encargada de guardar los objetos de tipo Employee y Employers.
+        /// También, de acuerdo al principio SRP, separamos la creación del Singleton (GenericList).
+        /// </summary>
+        /// <typeparam name="Employee"></typeparam>
+        /// <returns></returns>
         public static GenericList<Employee> Employees = new GenericList<Employee>();
         public static GenericList<Employer> Employers = new GenericList<Employer>();
 
+        /// <summary>
+        /// Tenemos un Polimorfismo en las operaciones pues no es necesario separar el método.
+        /// Hacer una misma operación con dos ingresos distintos permite más fluidez.
+        /// </summary>
+        /// <param name="employee"></param>
         public static void Register(Employee employee)
         {
             Employees.Register(employee);
